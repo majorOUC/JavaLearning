8e8adf7d65b2c478e872221d7c828afe6e6eb82d
Mengzhe Wang <15588753313@163.com>
2025-01-24 13:20:29 +0800

    Learning case for javase

--- loop/src/ouc/wangmengzhe/whileloop/whileloopdemo.java
+++ loop/src/ouc/wangmengzhe/whileloop/whileloopdemo.java
@@ -3,8 +3,15 @@ package ouc.wangmengzhe.whileloop;
 public class whileloopdemo {
     public static void main(String[] args) {
         //学习while循环
-
+        whileloop();
     }
 
+    public static void whileloop() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
 
 }
