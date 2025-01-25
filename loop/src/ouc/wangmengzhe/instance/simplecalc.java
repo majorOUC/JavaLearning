6d5021ff94390025e8456350f2fe86ae6aec2726
Mengzhe Wang <15588753313@163.com>
2025-01-25 12:03:00 +0800

    Learning case for javase

--- loop/src/ouc/wangmengzhe/instance/simplecalc.java
+++ loop/src/ouc/wangmengzhe/instance/simplecalc.java
@@ -3,6 +3,6 @@ package ouc.wangmengzhe.instance;
 public class simplecalc {
     public static void main(String[] args) {
         //设计一个简单计算器，从控制台读取用户输入的数字和运算符并且输出结果
-
+        System.out.println("请输入两个数字和运算符");
     }
 }
