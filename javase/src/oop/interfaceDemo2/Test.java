745b6cbd18f843559862b8921fb17fb274c0638f
Mengzhe Wang <15588753313@163.com>
2025-02-11 15:13:25 +0800

    Learning case for javase

--- javase/src/oop/interfaceDemo2/Test.java
+++ javase/src/oop/interfaceDemo2/Test.java
@@ -14,33 +14,7 @@ public class Test {
         //创建一个智能家居控制对象来对各种家具进行操作
         Intelligenthomecontrol ihc = Intelligenthomecontrol.getInstance();
 
-        while(true) {
-            for (int i = 0; i < f.length; i++) {
-                System.out.println((i+1) + f[i].getName() + "当前的状态是：" + f[i].getStatus());
-            }
-            System.out.println("请选择您要操作的设备(退出请输入exit)：");
-            Scanner sc = new Scanner(System.in);
-            String op = sc.next();
-            switch(op){
-                case "1":
-                    ihc.press(f[0]);
-                    break;
-                case "2":
-                    ihc.press(f[1]);
-                    break;
-                case "3":
-                    ihc.press(f[2]);
-                    break;
-                case "4":
-                    ihc.press(f[3]);
-                    break;
-                case "exit":
-                    System.out.println("退出系统");
-                    return;
-                default:
-                    System.out.println("输入有误，请重新输入");
-                    break;
-            }
-        }
+        function.op1(ihc, f);
+
     }
 }
