83f0e704b2a516d50a48981e8309e6b208360d95
Mengzhe Wang <15588753313@163.com>
2025-02-11 22:32:59 +0800

    Learning case for javase

---
+++ javase/src/oop/innerclass/innerclassDemo1.java
@@ -0,0 +1,36 @@
+package oop.innerclass;
+
+public class innerclassDemo1 {
+    public static void main(String[] args) {
+        //关于匿名内部类的常见使用形式和作用
+        //直接使用匿名内部类，实现接口，实现接口中的方法
+        //匿名内部类实际上是定义了一个子类，同时也创建了一个子类对象。
+        swim teacher = new swim() {
+            @Override
+            public void swim() {
+                System.out.println("老师在游泳");
+            }
+        };
+        swim student = new swim() {
+            @Override
+            public void swim() {
+                System.out.println("学生在游泳");
+            }
+        };
+        start(teacher);
+        System.out.println("=======================");
+        start(student);
+    }
+
+    //定义一个方法，来开始老师和学生的游泳动作，传入的是一个接口类型，因为不确定是老师还是学生，所以使用接口
+    public static void start(swim s){
+        System.out.println("开始游泳了=========");
+        s.swim();
+        System.out.println("游泳结束===========");
+    }
+}
+
+//定义一个接口，来规范老师和学生的游泳行为
+interface swim{
+    void swim();
+}
---
+++ javase/src/oop/innerclass/innerclassDemo2.java
@@ -0,0 +1,43 @@
+package oop.innerclass;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import oop.oopDemo1.Stu;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class innerclassDemo2 {
+    //匿名内部类的另一种应用场景-------使用comparator接口进行排序
+    public static void main(String[] args) {
+        Student[] students = new Student[6];
+        students[0] = new Student("张三", 18, 1.75, '男');
+        students[1] = new Student("李四", 19, 1.80, '男');
+        students[2] = new Student("王五", 20, 1.70, '男');
+        students[3] = new Student("赵六", 21, 1.65, '女');
+        students[4] = new Student("钱七", 22, 1.60, '女');
+        students[5] = new Student("孙八", 23, 1.55, '女');
+
+        //按照年龄进行升序排序
+        Arrays.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o2.getAge() - o1.getAge();
+            }
+        });
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Student{
+    private String name;
+    private int age;
+    private double height;
+    private char sex;
+}
